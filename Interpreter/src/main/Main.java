f1ed38c2d804229bd20b4ed1763894862f53a949
haibaradu <39399924+haibaradu@users.noreply.github.com>
2018-10-08 15:19:57 +0800

    Update Main.java

--- Interpreter/src/main/Main.java
+++ Interpreter/src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		Input input = new Input("/Users/Haoyu/eclipse-workspace/Interpreter/src/main/source.txt");
+		Input input = new Input("../Interpreter/src/main/source.txt");
 	    Analysis analysis = new Analysis(input);
 	    analysis.Lexical();
 	    analysis.print();
